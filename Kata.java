d518bea969c40aa838289cf3d366510d9c66a2c7
desdjajamahesa <desdjaja@yaoo.com>
2024-02-02 03:55:55 +0700

    Day 1/366 - EvenOrOdd

---
+++ Kata.java
@@ -0,0 +1,24 @@
+/**
+ * Java Coding Challenge
+ * Day 1/366
+ * https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+ */
+
+public class Kata {
+  // public static String evenOrOdd(int number) {
+  //   if(number % 2 == 0) {
+  //     return "Even";
+  //   } else {
+  //     return "Odd";
+  //   }
+  // }
+
+  // public static String evenOrOdd(int number) {
+  //   String result = (number % 2 == 0) ? "Even" : "Odd";
+  //   return result;
+  // }
+
+  public static String evenOrOdd(int number) {
+    return (number % 2 == 0) ? "Even" : "Odd";
+  }
+}
